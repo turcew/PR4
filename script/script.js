847a9eab2d4021733a34ba6b3e28fe7f97ed21e1
turcew <120652745+turcew@users.noreply.github.com>
2025-11-24 05:08:34 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,6 +4,83 @@ document.addEventListener('DOMContentLoaded', function() {
   const closeModal = document.querySelector('#closeModal');
   const questionTitle = document.querySelector('#question');
   const formAnswers = document.querySelector('#formAnswers');
+  const nextButton = document.querySelector('#next');
+  const prevButton = document.querySelector('#prev');
+  
+  const questions = [
+    {
+        question: "Якого кольору бургер?",
+        answers: [
+            {
+                title: 'Стандарт',
+                url: './image/burger.png'
+            },
+            {
+                title: 'Чорний',
+                url: './image/burgerBlack.png'
+            }
+        ],
+        type: 'radio'
+    },
+    {
+        question: "З якого м'яса котлета?",
+        answers: [
+            {
+                title: 'Курка',
+                url: './image/chickenMeat.png'
+            },
+            {
+                title: 'Яловичина',
+                url: './image/beefMeat.png'
+            },
+            {
+                title: 'Свинина',
+                url: './image/porkMeat.png'
+            }
+        ],
+        type: 'radio'
+    },
+    {
+        question: "Додаткові інгредієнти ?",
+        answers: [
+            {
+                title: 'Помідор',
+                url: './image/tomato.png'
+            },
+            {
+                title: 'Огірок',
+                url: './image/cucumber.png'
+            },
+            {
+                title: 'Салат',
+                url: './image/salad.png'
+            },
+            {
+                title: 'Цибуля',
+                url: './image/onion.png'
+            }
+        ],
+        type: 'checkbox'
+    },
+    {
+        question: "Додати соус?",
+        answers: [
+            {
+                title: 'Часниковий',
+                url: './image/sauce1.png'
+            },
+            {
+                title: 'Томатний',
+                url: './image/sauce2.png'
+            },
+            {
+                title: 'Гірчичний',
+                url: './image/sauce3.png'
+            }
+        ],
+        type: 'radio'
+    }
+];
 
   btnOpenModal.addEventListener('click', () => {
     modalBlock.classList.add('d-block');
@@ -15,26 +92,43 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   const playTest = () => {
-    const renderQuestion = () => {
-      questionTitle.textContent = 'Какого цвета бургер вы хотите';
-
-      formAnswers.innerHTML = `
+	  let numberQuestion = 0;
+	  
+    const renderAnswers = (index) => {
+      questions[index].answers.forEach((answer) => {
+		const answerItem = document.createElement('div');
+		
+		answerItem.classList.add('answer-item', 'd-flex', 'flex-column');
+		
+		answerItem.innerHTML = `
         <div class="answers-item d-flex flex-column">
-          <input type="radio" id="answerItem1" name="answer" class="d-none">
-          <label for="answerItem1" class="d-flex flex-column justify-content-between">
-            <img class="answerImg" src="./image/burger.png" alt="burger">
-            <span>Стандарт</span>
-          </label>
-        </div>
-        <div class="answers-item d-flex flex-column">
-          <input type="radio" id="answerItem2" name="answer" class="d-none">
-          <label for="answerItem2" class="d-flex flex-column justify-content-between">
-            <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
-            <span>Чорний</span>
+          <input type="radio" id="$[answer.title]" name="answer" class="d-none">
+          <label for="$[answer.title]" class="d-flex flex-column justify-content-between">
+            <img class="answerImg" src="$[answer.url]" alt="burger">
+            <span>$[answer.title]</span>
           </label>
         </div>
       `;
+	  formAnswers.appendChild(answerItem);
+	  })
     };
-    renderQuestion();
+    const renderQuestion = (indexQuestion) => {
+		formAnswers.innerHTML = '';
+		
+		questionTitle.textContent = '${question[indexQuestion].question}';
+		
+		renderAnswers(indexQuestion);
+	}
+	renderQuestion(numberQuestion);
+	
+	nextButton.onclick = () => {
+		numberQuestion++;
+		renderQuestion(numberQuestion);
+	}
+	
+	prevButton.onclick = () => {
+		numberQuestion--;
+		renderQuestion(numberQuestion);
+	}
   };
 });
